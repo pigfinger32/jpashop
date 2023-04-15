fe316ace449ae8a6c3a30c1d012c1c3efd6625ab
wnghks517 <wnghks517@gmail.com>
2023-04-15 13:40:03 +0900

    Commit test 2023-04-15

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,6 +19,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
+    //@ManyToOne(fetch = LAZY)로 변경 2023-04-15
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
