91213edaf7271b0abefda3991190907fb4560ebe
wnghks517 <wnghks517@gmail.com>
2023-04-24 23:43:39 +0900

    회원가입 회원조회 상품등록 상품조회 까지. ________ 다음 상품 수정 들어갈 예정

---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus;//주문 상태[ORDER, CANCEL]
+
+
+}
