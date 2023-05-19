c7b26bccb510dc2998ecf35092844af03bcf66ab
wnghks517 <wnghks517@gmail.com>
2023-05-19 14:42:08 +0900

    Login시도중...

--- src/main/java/jpabook/jpashop/domain/MemberDAOImpl.java
+++ src/main/java/jpabook/jpashop/domain/MemberDAOImpl.java
@@ -5,9 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MemberDAOImpl implements MemberDAO{
 
     @Autowired
-    SqlSession sqlSession; //Sqlsession 의존관계 주입
     @Override
     public String loginCheck(MemberDTO dto) {
-        return
+        return null;
     }
 }
