5562781f23ff2872b8c6c0ba79add0ea861a41d0
wnghks517 <wnghks517@gmail.com>
2023-09-01 14:41:12 +0900

    nojup -> nohup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,7 @@ echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-nojup java -jar \
+nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
